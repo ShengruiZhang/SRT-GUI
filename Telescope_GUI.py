d803dae69098f0b757f2f0a0d8183a324599a662
Shengrui <msi1459481096@gmail.com>
2020-11-26 18:17:25 -0700

    V0.1

--- Telescope_GUI.py
+++ Telescope_GUI.py
@@ -1,7 +1,11 @@
 import PySimpleGUI as sg
 
 layout = [  [sg.T('Radio Telescope Control',size=(70,1),font='FreeSans 24',justification='center')],
-            [sg.T('Status: ')]
+            [sg.T('Telescope Status: ',font='FreeSans 16',justification='left',pad=((10,0),(0,0)))],
+            [sg.T('')],
+            [sg.T('Current Alt Position: ',font='FreeSans 14'), sg.T('',font='FreeSans 16',key='_ALT-POS_')],
+            [sg.T('Current Az Position: ',font='FreeSans 14')],
+            [sg.B('Commit')]
             ]
 
 window = sg.Window('Radio Telescope Control Interface Version Beta v0.1', layout)
@@ -11,3 +15,5 @@ while True:
     print(event)
     if event == sg.WIN_CLOSED:
         break
+    if event == 'Commit':
+        window['_ALT-POS_'].update(1)
