bffa166499ab07fc01d84795de9770931e29d80b
carsto08 <73854977+carsto08@users.noreply.github.com>
2020-11-29 16:30:57 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+import serial
 
 #sg.theme('LightGrey5')
 sg.theme_input_background_color('ghost white')
@@ -8,42 +9,67 @@ sg.theme_text_color('ghost white')
 #sg.theme_button_color('midnight blue')
 
 
-
 # ------ Menu Definition ------ #
 menu_def = [['&File', ['&Open', '&Save', 'E&xit', 'Properties']],
             ['&Edit', ['Paste', ['Special', 'Normal', ], 'Undo'], ],
             ['&Help', '&About...'], ]
 
+coord_entry = [ [sg.Text('Coordinate Entry: ', font=("Helvetica", 12, 'underline bold'))],
+           [sg.Text('Altitudinal:'), sg.Text(size=(15,1), font=("Helvetica", 10), key='-OUTPUT-')],
+           [sg.Input(key='-IN-', size=(15,1))],
+           [sg.Text('Azimuthal:'), sg.Text(size=(15,1), font=("Helvetica", 10), key='-OUTPUT2-')],
+           [sg.Input(key='-IN2-', size=(15,1))],
+           [sg.OK()]]
+
+parameters = [ [sg.Text('Parameters: ', font=("Helvetica", 12, 'underline bold'))],
+           [sg.Text('Current Position:', font=("Helvetica", 10, 'underline'))],
+           [sg.Text('32 degree N, 10 degree W', font=("Helvetica", 10))],
+           [sg.Text('Target Position:', font=("Helvetica", 10, 'underline'))],
+           [sg.Text('59 degree N, 64 degree W')],
+               [sg.Text(' ')]
+               ]
+
+motor_status = [[sg.Text('Motor Status: ', font=("Helvetica", 12, 'underline bold'))],
+               [sg.Text('Voltage:', font=("Helvetica", 10, 'underline'))],
+                [sg.Text('Current:', font=("Helvetica", 10, 'underline'))],
+               [sg.Text('Temperature:', font=("Helvetica", 10, 'underline'))],
+                [sg.Text(' ')],
+                [sg.Text(' ')]
+                ]
 # layout
 layout = [ [sg.Menu(menu_def, tearoff=True)],
            [sg.Text('Student Radio Telescope', size=(30,1), justification='center', font=("Helvetica", 25), relief=sg.RELIEF_FLAT)],
-           [sg.Text(" ")],
-           [sg.Button('Start Calibration', size=(20,1), font=("Helvetica", 15)) ],
-          # [sg.Text(" ")],
-           [sg.Frame(layout=[
+          [sg.Text(" ")],
+           [sg.Button('Start Calibration', size=(15,1), font=("Helvetica", 13)), sg.Button('EMERGENCY STOP!!', size=(20,1), font=("Helvetica", 15))],
+            [sg.Text(" ")],
+           [sg.Column(coord_entry, element_justification='c'), sg.VSeparator(), sg.Column(parameters, element_justification='c'), sg.VSeparator(), sg.Column(motor_status, element_justification = 'l')],
+          [sg.Text(" ")],
+          # [sg.Frame(layout=[
           # [sg.Text('Manual Coordinate Entry:')],
-           [sg.Text('Altitudinal:'), sg.Text(size=(15,1), font=("Helvetica", 10), key='-OUTPUT-')],
-           [sg.Input(key='-IN-')],
-           [sg.Text('Azimuthal:'), sg.Text(size=(15,1), font=("Helvetica", 10), key='-OUTPUT2-')],
-           [sg.Input(key='-IN2-')],
-           [sg.OK()]], title= 'Manual Coordinate Entry: ', font=("Helvetica", 12), title_color='white', relief=sg.RELIEF_RIDGE, background_color='maroon')],
-           [sg.Frame(layout=[
-           [sg.Button('STOP!!!!', size=(20,1), font=("Helvetica", 15))]], title= 'Emergency Stop: ', font=("Helvetica", 12), title_color='white', relief=sg.RELIEF_RIDGE, background_color='maroon')],
-           [sg.Frame(layout=[
-           [sg.Text('Current Position:', font=("Helvetica", 10, 'underline bold'))],
-           [sg.Text('32 degree N, 10 degree W', font=("Helvetica", 10))],
-           [sg.Text('Target Position:', font=("Helvetica", 10, 'underline bold'))],
-           [sg.Text('59 degree N, 64 degree W')]], title= 'Positioning: ', font=("Helvetica", 12), title_color='white', relief=sg.RELIEF_RIDGE, background_color='maroon')],
+         #  [sg.Text('Altitudinal:'), sg.Text(size=(15,1), font=("Helvetica", 10), key='-OUTPUT-')],
+         #  [sg.Input(key='-IN-', size=(15,1))],
+         #  [sg.Text('Azimuthal:'), sg.Text(size=(15,1), font=("Helvetica", 10), key='-OUTPUT2-')],
+         #  [sg.Input(key='-IN2-', size=(15,1))],
+        #   [sg.OK()]], title= 'Coordinate Entry: ', font=("Helvetica", 12), size=(25, 25), title_color='white', relief=sg.RELIEF_RIDGE, background_color='maroon')],
+         #  [sg.Frame(layout=[
+         #  [sg.Button('EMERGENCY STOP!!', size=(20,1), font=("Helvetica", 15))]], title= 'Emergency Stop: ', font=("Helvetica", 12), title_color='white', relief=sg.RELIEF_RIDGE, background_color='maroon')],
+         #  [sg.Frame(layout=[
+         #  [sg.Text('Current Position:', font=("Helvetica", 10, 'underline bold'))],
+         #  [sg.Text('32 degree N, 10 degree W', font=("Helvetica", 10))],
+         #  [sg.Text('Target Position:', font=("Helvetica", 10, 'underline bold'))],
+         #  [sg.Text('59 degree N, 64 degree W')],
+         #  [sg.Text('Motor Status: ', font=("Helvetica", 10, 'underline bold'))],
+         #      [sg.Text('Voltage:     ')],
+          #     [sg.Text('Temperature:     ')],
+         #      [sg.Text('Current:     ')],
+         #  ], title= 'Parameters: ', font=("Helvetica", 12), title_color='white', relief=sg.RELIEF_RIDGE, background_color='maroon')],
            #  [sg.Text('Start Calibration Routine:')],
-           [sg.Frame(layout=[
-           [sg.Radio('Amplitude vs. Time', "RADIO1", default=True, background_color='maroon', font=("Helvetica", 10))],
-           [sg.Radio('Amplitude vs. Position', "RADIO1", background_color='maroon', font=("Helvetica", 10))],
-           [sg.Button('Output Graph')]], title= 'Graphing Options: ', font=("Helvetica", 12), title_color='white', relief=sg.RELIEF_RIDGE, background_color='maroon', element_justification='c')],
           # [sg.Graph(canvas_size= 100, graph_top_right=10, graph_bottom_left=10)],
            [sg.Frame(layout=[
+           [sg.Button('Start Recording Data', font=("Helvetica", 10)), sg.Button('Stop Recording Data', font=("Helvetica", 10))],
            [sg.Text('Choose A Folder to Save Your Graph To:', size=(35, 1), font=("Helvetica", 10))],
            [sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),
-            sg.InputText('Default Folder', font=("Helvetica", 10)), sg.FolderBrowse()]], title= 'Save Data: ', font=("Helvetica", 12), title_color='white', relief=sg.RELIEF_RIDGE, background_color='maroon', element_justification='c')],
+            sg.InputText('Default Folder', font=("Helvetica", 10)), sg.FolderBrowse()]], title= 'Data Output: ', font=("Helvetica", 12), title_color='white', relief=sg.RELIEF_RIDGE, background_color='maroon', element_justification='c')],
            [sg.Exit()]]
           # [sg.OK(), sg.Cancel()]]
 
