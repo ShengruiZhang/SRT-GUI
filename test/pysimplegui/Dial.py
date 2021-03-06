253013b9df777e8fac7800f6512b30af00bc0775
Shengrui <msi1459481096@gmail.com>
2021-03-06 03:22:36 -0700

    finished dial

--- test/pysimplegui/Dial.py
+++ test/pysimplegui/Dial.py
@@ -1,3 +1,5 @@
+import time
+import math
 import PySimpleGUI as sg
 
 layout = [
@@ -24,13 +26,6 @@ Dial.DrawCircle( (0,100), 500,
 Dial.DrawPoint( (0,100),
                 size=25,
                 color='gold')
-
-Dial.DrawText("AZ Position = 100\N{DEGREE SIGN}",
-                (0,-600),
-                color='gold',
-                font=("OpenSans 36"),
-                angle=0,
-                text_location="center")
 
 Dial.DrawText(  "0",
                 (0,650),
@@ -88,20 +83,57 @@ Dial.DrawText(  "E",
                 angle=0,
                 text_location="center")
 
-Dial.DrawLine(  (0,100),
-                (354,554),
-                color='gold',
-                width=3)
+line = Dial.DrawLine(   (0,100),
+                        (0,600),
+                        color='gold',
+                        width=3)
 
-Dial.DrawPoint( (355,555),
-                size=25,
-                color='lime green')
+point = Dial.DrawPoint( (0,600),
+                        size=25,
+                        color='lime green')
+
+degree = Dial.DrawText("AZ Position = 0\N{DEGREE SIGN}",
+                        (0,-600),
+                        color='gold',
+                        font=("OpenSans 36"),
+                        angle=0,
+                        text_location="center")
+
+input_deg = 0
 
 while True:
 
-    event, values = window.read(timeout=100)
+    event, values = window.read(timeout=50)
 
     if event == sg.WIN_CLOSED:
         break
+
+    dial_y = 500 * math.cos(abs(input_deg)*math.pi/180)
+    dial_x = 500 * math.sin(abs(input_deg)*math.pi/180)
+
+    Dial.delete_figure(line)
+    Dial.delete_figure(point)
+    Dial.delete_figure(degree)
+
+    line = Dial.DrawLine(   (0,100),
+                            (dial_x,dial_y+100),
+                            color='gold',
+                            width=3)
+
+    point = Dial.DrawPoint( (dial_x,dial_y+100),
+                            size=25,
+                            color='lime green')
+
+    degree = Dial.DrawText(f'AZ Position = {abs(input_deg)}\N{DEGREE SIGN}',
+                        (0,-600),
+                        color='gold',
+                        font=("OpenSans 36"),
+                        angle=0,
+                        text_location="center")
+
+    if input_deg <= 360:
+        input_deg += 1
+    else:
+        input_deg = -360
 
 window.close()
